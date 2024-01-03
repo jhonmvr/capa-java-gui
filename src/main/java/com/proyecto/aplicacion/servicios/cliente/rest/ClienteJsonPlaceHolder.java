3dcf81eb3ec7f53f03444b91c157d9f478d950f2
luis.tamayo <luis.tamayo>
2024-01-02 21:49:28 -0500

    fix package

--- src/main/java/com/proyecto/aplicacion/servicios/cliente/rest/ClienteJsonPlaceHolder.java
+++ src/main/java/com/proyecto/aplicacion/servicios/cliente/rest/ClienteJsonPlaceHolder.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.proyecto.aplicacion.proyecto.servicios.cliente.rest;
+package com.proyecto.aplicacion.servicios.cliente.rest;
 
 import java.io.BufferedReader;
 import java.io.IOException;
